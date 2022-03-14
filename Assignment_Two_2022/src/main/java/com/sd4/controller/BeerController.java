57416bc471106303f4d9b096923afa170eae26b6
Pawel <pindel.pawel@hotmail.com>
2022-03-14 19:51:32 +0000

    added HATEOAS functionality

--- Assignment_Two_2022/src/main/java/com/sd4/controller/BeerController.java
+++ Assignment_Two_2022/src/main/java/com/sd4/controller/BeerController.java
@@ -6,10 +6,20 @@ package com.sd4.controller;
 
 import com.sd4.model.Beer;
 import com.sd4.service.BeerService;
+import java.awt.PageAttributes.MediaType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpStatus;
+//import org.springframework.hateoas.server.mvc.ControllerLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+//import static org.sfw.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +27,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,16 +35,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @author paw
  */
 @RestController
+@RequestMapping("/beers")
 public class BeerController {
     @Autowired
     private BeerService beerService;
     
-    @GetMapping("/beers")
+    //@GetMapping(value = "/hateoas/{id}", produces = { "application/hal+json" })
+    @GetMapping(value = "/hateoas/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<Beer> getOneWithHATEOAS(@PathVariable long id) {
+       Optional<Beer> o =  beerService.findOne(id);
+       
+       if (!o.isPresent()) 
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+         else {
+           Link selfLink = linkTo(methodOn(BeerController.class)
+                   .getAll()).withRel("allBeers");
+           o.get().add(selfLink);
+            return ResponseEntity.ok(o.get());
+       }
+    }
+    
+    @GetMapping("")
     public List<Beer> getAll() {
         return beerService.findAll();
     }
     
-    @GetMapping("/beers/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Beer> getOne(@PathVariable long id) {
        Optional<Beer> o =  beerService.findOne(id);
        
@@ -43,24 +70,24 @@ public class BeerController {
             return ResponseEntity.ok(o.get());
     }
     
-    @GetMapping("/beers/count")
+    @GetMapping("count")
     public long getCount() {
         return beerService.count();
     }
     
-    @DeleteMapping("/beers/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity delete(@PathVariable long id) {
         beerService.deleteByID(id);
         return new ResponseEntity(HttpStatus.OK);
     }
     
-    @PostMapping("/beers/")
+    @PostMapping("")
     public ResponseEntity add(@RequestBody Beer a) {
         beerService.saveBeer(a);
         return new ResponseEntity(HttpStatus.CREATED);
     }
     
-    @PutMapping("/beers/")
+    @PutMapping("")
     public ResponseEntity edit(@RequestBody Beer a) { //the edit method should check if the Author object is already in the DB before attempting to save it.
         beerService.saveBeer(a);
         return new ResponseEntity(HttpStatus.OK);
